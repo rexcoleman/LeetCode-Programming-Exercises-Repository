a2b090b97ef580b3d962be3e8532e9abb94af452
Rex Coleman <rexcoleman@gmail.com>
2023-07-30 22:51:01 -0700

    Commit exercises 19 - 20

---
+++ Exercise19-LinkedListRemoveDuplicates.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def print_all(self):
+        if self.length == 0:
+            print("Head: None")
+        else:
+            print("Head: ", self.head.value)
+        print("Length: ", self.length)
+        print("\nLinked List:")
+        if self.length == 0:
+            print("empty")
+        else:
+            self.print_list()
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+
+    def remove_duplicates(self):
+        values = set()
+        previous = None
+        current = self.head
+        while current:
+            if current.value in values:
+                previous.next = current.next
+                self.length -= 1
+            else:
+                values.add(current.value)
+                previous = current
+            current = current.next
+        print(values)
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(3)
+my_linked_list.append(2)
+my_linked_list.append(4)
+my_linked_list.remove_duplicates()
+
+my_linked_list.print_all()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head:  1
+    Length:  4
+    Linked List:
+    1
+    2
+    3
+    4
+
+"""
---
+++ Exercise20-LinkedLIstBinaryToDecimal.py
@@ -0,0 +1,113 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def print_all(self):
+        if self.length == 0:
+            print("Head: None")
+        else:
+            print("Head: ", self.head.value)
+        print("Length: ", self.length)
+        print("\nLinked List:")
+        if self.length == 0:
+            print("empty")
+        else:
+            self.print_list()
+
+    def binary_to_decimal(self):
+        current = self.head
+        num = 0
+        while current:
+            num = (num * 2) + current.value
+            current = current.next
+        return num
+
+
+# Test case 1: Binary number 110 = Decimal number 6
+linked_list = LinkedList(1)
+linked_list.append(1)
+linked_list.append(0)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 6
+    print("Test case 1 passed, returned: ", result)
+except AssertionError:
+    print("Test case 1 failed, returned: ", result)
+
+# Test case 2: Binary number 1000 = Decimal number 8
+linked_list = LinkedList(1)
+linked_list.append(0)
+linked_list.append(0)
+linked_list.append(0)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 8
+    print("Test case 2 passed, returned: ", result)
+except AssertionError:
+    print("Test case 2 failed, returned: ", result)
+
+# Test case 3: Binary number 0 = Decimal number 0
+linked_list = LinkedList(0)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 0
+    print("Test case 3 passed, returned: ", result)
+except AssertionError:
+    print("Test case 3 failed, returned: ", result)
+
+# Test case 4: Binary number 1 = Decimal number 1
+linked_list = LinkedList(1)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 1
+    print("Test case 4 passed, returned: ", result)
+except AssertionError:
+    print("Test case 4 failed, returned: ", result)
+
+# Test case 5: Binary number 1101 = Decimal number 13
+linked_list = LinkedList(1)
+linked_list.append(1)
+linked_list.append(0)
+linked_list.append(1)
+result = linked_list.binary_to_decimal()
+try:
+    assert result == 13
+    print("Test case 5 passed, returned: ", result)
+except AssertionError:
+    print("Test case 5 failed, returned: ", result)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Test case 1 passed, returned:  6
+    Test case 2 passed, returned:  8
+    Test case 3 passed, returned:  0
+    Test case 4 passed, returned:  1
+    Test case 5 passed, returned:  13
+"""
+
