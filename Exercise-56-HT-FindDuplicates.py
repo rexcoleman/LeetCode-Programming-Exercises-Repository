01c810a40f74eb97d23d7be22ecd06ae3a497937
Rex Coleman <rexcoleman@gmail.com>
2023-08-02 12:34:32 -0700

    Commit exercise 56

---
+++ Exercise-56-HT-FindDuplicates.py
@@ -0,0 +1,33 @@
+# WRITE FIND_DUPLICATES FUNCTION HERE #
+#                                     #
+#                                     #
+#                                     #
+#                                     #
+#######################################
+
+
+
+
+print ( find_duplicates([1, 2, 3, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 3]) )
+print ( find_duplicates([1, 1, 1, 1, 1]) )
+print ( find_duplicates([1, 2, 3, 3, 3, 4, 4, 5]) )
+print ( find_duplicates([1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]) )
+print ( find_duplicates([]) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    []
+    [1, 2]
+    [1]
+    [3, 4]
+    [1, 2, 3]
+    [1, 2, 3]
+    []
+
+"""
+
