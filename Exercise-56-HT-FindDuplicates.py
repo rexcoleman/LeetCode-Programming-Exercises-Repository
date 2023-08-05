8361c866df0ff12fc79fbceb4c9c024eec867fea
Rex Coleman <rexcoleman@gmail.com>
2023-08-05 15:05:38 -0700

    Complete exercise 56

--- Exercise-56-HT-FindDuplicates.py
+++ Exercise-56-HT-FindDuplicates.py
@@ -1,11 +1,20 @@
-# WRITE FIND_DUPLICATES FUNCTION HERE #
-#                                     #
-#                                     #
-#                                     #
-#                                     #
-#######################################
+def find_duplicates(nums):
+    num_counts = {}
+    for num in nums:
+        num_counts[num] = num_counts.get(num, 0) + 1
+    duplicates = [num for num, count in num_counts.items() if count > 1]
+    return duplicates
 
 
+# def find_duplicates(list1):
+#     list2 = []
+#     list3 = []
+#     for i in list1:
+#         if i in list2:
+#             if i not in list3:
+#                 list3.append(i)
+#         list2.append(i)
+#     return list3
 
 
 print ( find_duplicates([1, 2, 3, 4, 5]) )
