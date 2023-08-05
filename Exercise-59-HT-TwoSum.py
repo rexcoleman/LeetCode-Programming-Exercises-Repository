03ee2e12bb063a780651918814c7b0e88674bb79
Rex Coleman <rexcoleman@gmail.com>
2023-08-05 16:06:54 -0700

    Complete exercise 59

--- Exercise-59-HT-TwoSum.py
+++ Exercise-59-HT-TwoSum.py
@@ -1,9 +1,11 @@
-# WRITE TWO_SUM FUNCTION HERE #
-#                             #
-#                             #
-#                             #
-#                             #
-###############################
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []
 
 
 print(two_sum([5, 1, 7, 2, 9, 3], 10))
