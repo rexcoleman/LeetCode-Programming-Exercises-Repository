4866f9614cef7e08269a80200bcc1f21fee33748
Rex Coleman <rexcoleman@gmail.com>
2023-08-05 15:42:34 -0700

    Complete exercise 58

--- Exercise-58-HT-GroupAnagrams.py
+++ Exercise-58-HT-GroupAnagrams.py
@@ -1,11 +1,12 @@
-# WRITE GROUP_ANAGRAMS FUNCTION HERE #
-#                                    #
-#                                    #
-#                                    #
-#                                    #
-######################################
-
-
+def group_anagrams(strings):
+    anagram_groups = {}
+    for string in strings:
+        canonical = ''.join(sorted(string))
+        if canonical in anagram_groups:
+            anagram_groups[canonical].append(string)
+        else:
+            anagram_groups[canonical] = [string]
+    return list(anagram_groups.values())
 
 
 print("1st set:")
