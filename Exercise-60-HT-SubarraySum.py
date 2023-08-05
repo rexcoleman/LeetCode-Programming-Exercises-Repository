75842e2a736181da0e0ceb59346e66e82ae37a22
Rex Coleman <rexcoleman@gmail.com>
2023-08-05 16:30:50 -0700

    Complete exercise 60

--- Exercise-60-HT-SubarraySum.py
+++ Exercise-60-HT-SubarraySum.py
@@ -1,9 +1,14 @@
-# WRITE SUBARRAY_SUM FUNCTION HERE #
-#                                  #
-#                                  #
-#                                  #
-#                                  #
-####################################
+def subarray_sum(nums, target):
+    sum_index = {0: -1}
+    current_sum = 0
+    for i, num in enumerate(nums):
+        current_sum += num
+        print(current_sum)
+        print(sum_index)
+        if current_sum - target in sum_index:
+            return [sum_index[current_sum - target] + 1, i]
+        sum_index[current_sum] = i
+    return []
 
 
 
