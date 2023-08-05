b028e4d9185d7bb9d573045055fda1aec9f87e24
Rex Coleman <rexcoleman@gmail.com>
2023-08-05 15:17:48 -0700

    Complete exercise 57

--- Exercise-57-HT-FirstNonRepeatingCharacter.py
+++ Exercise-57-HT-FirstNonRepeatingCharacter.py
@@ -1,11 +1,11 @@
-# WRITE THE FUNCTION HERE #
-#                         #
-#                         #
-#                         #
-#                         #
-###########################
-
-
+def first_non_repeating_char(string):
+    char_counts = {}
+    for char in string:
+        char_counts[char] = char_counts.get(char, 0) + 1
+    for char in string:
+        if char_counts[char] == 1:
+            return char
+    return None
 
 print( first_non_repeating_char('leetcode') )
 
