105fe0ab91249b76c915a7505ed0f5d3e71a72c5
Rex Coleman <rexcoleman@gmail.com>
2023-08-06 08:52:25 -0700

    Complete exercise 61

---
+++ Exercise-61-Set-RemoveDuplicates.py
@@ -0,0 +1,21 @@
+def remove_duplicates(my_list):
+    # my_set = ([list])
+    # return my_set
+    new_list = list(set(my_list))
+    return new_list
+
+
+my_list = [1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 4, 8, 9, 5]
+new_list = remove_duplicates(my_list)
+print(new_list)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    (Order may be different as sets are unordered)
+
+"""
