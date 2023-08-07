bba18b77f69887ca5f3d7da2d0f8d77abd0edad0
Rex Coleman <rexcoleman@gmail.com>
2023-08-07 12:40:41 -0700

    Complete exercise 72

---
+++ Exercise-72-<>MinHeap-Insert.py
@@ -0,0 +1,48 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+        while current > 0 and self.heap[current] < self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+
+myheap = MinHeap()
+myheap.insert(12)
+myheap.insert(10)
+myheap.insert(8)
+myheap.insert(6)
+
+print(myheap.heap)  # [6, 8, 10, 12]
+
+myheap.insert(4)
+
+print(myheap.heap)  # [4, 6, 10, 12, 8]
+
+myheap.insert(2)
+
+print(myheap.heap)  # [2, 6, 4, 12, 8, 10]
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [6, 8, 10, 12]
+    [4, 6, 10, 12, 8]
+    [2, 6, 4, 12, 8, 10]
+
+"""
