f65162386db7db9f1edc46f375bc8c67308eb21c
Rex Coleman <rexcoleman@gmail.com>
2023-09-12 11:22:50 -0700

    Complete Course 2 - Exercises 3

--- 2-Exercise-3-IntegerReversion.py
+++ 2-Exercise-3-IntegerReversion.py
@@ -1,19 +1,13 @@
 def reverse_integer(n):
-    # your implementation goes here
+    reversed_integer = 0
+    remainder = 0
+    while n > 0:
+        remainder = n % 10
+        n = n // 10
+        reversed_integer = (reversed_integer * 10) + remainder
+    return reversed_integer
 
-def reverse(data):
-    # convert string into a list of characters
-    data = list(data)
-    # pointing to the first item
-    start_index = 0
-    # index pointing to the last item
-    end_index = len(data) - 1
-    while end_index > start_index:
-        data[start_index], data[end_index] = data[end_index], data[start_index]
-        start_index += 1
-        end_index -= 1
-    # transform the list of letters into a string
-    return ''.join(data)
 
 if __name__ == '__main__':
-    print(is_palindrome('madam'))
+    print(reverse_integer(1234))
+
