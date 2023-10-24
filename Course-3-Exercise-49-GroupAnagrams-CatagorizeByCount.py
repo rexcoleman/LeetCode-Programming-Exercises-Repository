557baceb4ae74b70037991d001aa81a2dcb241a4
Rex Coleman <rexcoleman@gmail.com>
2023-10-23 19:24:01 -0700

    Course 3 - Update multiple exercises

---
+++ Course-3-Exercise-49-GroupAnagrams-CatagorizeByCount.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+
+
+
+
+if __name__ == '__main__':
+
+    # Inputs
+    strs_1 = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    strs_2 = [""]
+    strs_3 = ["a"]
+
+    # Run tests
+    solution_1 = Solution()
+    solution_2 = Solution()
+    solution_3 = Solution()
+
+    test_1 = solution_1.groupAnagrams(strs_1)
+    test_2 = solution_2.groupAnagrams(strs_2)
+    test_3 = solution_3.groupAnagrams(strs_3)
+
+    print(f"Test 1: {test_1}")
+    print('Expected Output 1: [["bat"],["nat","tan"],["ate","eat","tea"]]')
+    print(f"Test 2: {test_2}")
+    print('Expected Output 2: [[""]]')
+    print(f"Test 2: {test_3}")
+    print('Expected Output 3: [["a"]]')
