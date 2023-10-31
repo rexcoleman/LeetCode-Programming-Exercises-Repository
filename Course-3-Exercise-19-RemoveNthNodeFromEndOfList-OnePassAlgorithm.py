2afeff2fed2291384fa79c8688c906f015bae8f6
Rex Coleman <rexcoleman@gmail.com>
2023-10-31 12:01:31 -0700

    Course 3 - Update multiple exercises

--- Course-3-Exercise-19-RemoveNthNodeFromEndOfList-OnePassAlgorithm.py
+++ Course-3-Exercise-19-RemoveNthNodeFromEndOfList-OnePassAlgorithm.py
@@ -6,31 +6,31 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
-    def getLengthOfLL(self, head: Optional[ListNode]) -> int:
-        length = 0
-        while head:
-            length += 1
-            head = head.next
-        return length
-
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        temp = head
-        length = self.getLengthOfLL(temp)
-        if n == length:
+        fast = head
+        slow = head
+
+        # This separates the two pointers by n (fast - slow == n)
+        while n:
+            fast = fast.next
+            n -= 1
+
+        # This is the case where we are removing the head (fast = length)
+        if not fast:
             return head.next
-        if length == 1:
-            return None
 
-        i = 1
+        # This moves both pointers to the end of the list with fast - slow == n
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
 
-        while temp and i < (length - n):
-            temp = temp.next
-            i += 1
-
-        temp.next = temp.next.next
-
+        # Change slow.next to remove Nth node from end
+        slow.next = slow.next.next
         return head
+
+
 
 
 
@@ -46,9 +46,10 @@ def listToLinkedList(values):
     return head
 
 def linkedListToList(head):
+    output = []
     if not head:
         return []
-    output = []
+
     while head:
         output.append(head.val)
         head = head.next
@@ -60,13 +61,13 @@ if __name__ == '__main__':
 
     # Inputs
     head_1 = [1, 2, 3, 4, 5]
-    n_1 = 2
+    n_1 = 4
     head_2 = [1]
     n_2 = 1
     head_3 = [1, 2]
     n_3 = 1
 
-    # Create linked lists
+    # Transform data from lists to linked lists
     ll_head_1 = listToLinkedList(head_1)
     ll_head_2 = listToLinkedList(head_2)
     ll_head_3 = listToLinkedList(head_3)
@@ -76,23 +77,22 @@ if __name__ == '__main__':
     print(f"\nList 2: {linkedListToList(ll_head_2)} \nOriginal List: {head_2}")
     print(f"\nList 3: {linkedListToList(ll_head_3)} \nOriginal List: {head_3}")
 
-    # Create solution instance for each test
+    # Create solution objects for each test
     solution_1 = Solution()
     solution_2 = Solution()
     solution_3 = Solution()
 
-    # Remove Nth From End
+    # Remove Nth node from end
     test_1 = solution_1.removeNthFromEnd(ll_head_1, n_1)
     test_2 = solution_2.removeNthFromEnd(ll_head_2, n_2)
     test_3 = solution_3.removeNthFromEnd(ll_head_3, n_3)
 
-    # Transform data back to list format for output
+    # Tranfrom data from linked lists to lists
     output_1 = linkedListToList(test_1)
     output_2 = linkedListToList(test_2)
     output_3 = linkedListToList(test_3)
 
-    # Print test results
+    # Print results
     print(f"\nTest 1 Output: {output_1} \nExpected Output: [1,2,3,5]")
     print(f"\nTest 2 Output: {output_2} \nExpected Output: []")
     print(f"\nTest 3 Output: {output_3} \nExpected Output: [1]")
-
