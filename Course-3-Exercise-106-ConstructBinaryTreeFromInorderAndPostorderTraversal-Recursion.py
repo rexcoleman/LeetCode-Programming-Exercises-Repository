21aab9450d2d92a047b4f924d34feccdfe759966
Rex Coleman <rexcoleman@gmail.com>
2023-11-02 20:58:39 -0700

    Course 3 - Update multiple exercises

--- Course-3-Exercise-106-ConstructBinaryTreeFromInorderAndPostorderTraversal-Recursion.py
+++ Course-3-Exercise-106-ConstructBinaryTreeFromInorderAndPostorderTraversal-Recursion.py
@@ -7,36 +7,38 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
 
+
+class Solution:
+    def buildTree(self, postorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         def array_to_tree(left, right):
-            nonlocal preorder_index
+            nonlocal postorder_index
             # If there are no elements to construct the tree
             if left > right:
                 return None
-
-            # Select the preorder_index element as the root and increment it
-            root_value = preorder[preorder_index]
+            # Select the postorder_index element as the root and increment it
+            root_value = postorder[postorder_index]
             root = TreeNode(root_value)
 
-            preorder_index += 1
+            postorder_index -= 1
 
-            # Build left and right subtree
+            # Build right and left subtree
             # excluding inorder_index_map[root_value] element because it's the root
-            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
             root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
+            root.left = array_to_tree(left, inorder_index_map[root_value] -1)
 
             return root
 
-        preorder_index = 0
-
+        postorder_index = len(postorder) - 1
         # Build a hashmap to store value -> its index relations
         inorder_index_map = {}
         for index, value in enumerate(inorder):
             inorder_index_map[value] = index
 
-        return array_to_tree(0, len(preorder) - 1)
+        return array_to_tree(0, len(postorder) - 1)
+
+
+
 
 def tree_to_list(root):
     output = []
@@ -61,25 +63,21 @@ def tree_to_list(root):
 if __name__ == '__main__':
 
     # Inputs
-    preorder_1 = [3, 9, 20, 15, 7]
     inorder_1 = [9, 3, 15, 20, 7]
-    # Output_1 = [3,9,20,null,null,15,7]
-    preorder_2 = [-1]
+    postorder_1 = [9, 15, 7, 20, 3]
+    output_1 = [3,9,20,None,None,15,7]
     inorder_2 = [-1]
-    # Output_2 = [-1]
+    postorder_2 = [-1]
+    output_2 = [-1]
 
     solution_1 = Solution()
     solution_2 = Solution()
 
-    test_1 = solution_1.buildTree(preorder_1, inorder_1)
-    test_2 = solution_1.buildTree(preorder_2, inorder_2)
+    test_1 = solution_1.buildTree(postorder_1, inorder_1)
+    test_2 = solution_1.buildTree(inorder_2, postorder_2)
 
     output_1 = tree_to_list(test_1)
     output_2 = tree_to_list(test_2)
 
-    print(f"\nOutput 1: {output_1}: \nExpected Output: [3,9,20,None,None,15,7]")
-    print(f"\nOutput 2: {output_2}: \nExpected Output: [-1]")
-
-
-
-
+    print(f"\nOutput 1: {output_1}: \nExpected Output: {output_1}")
+    print(f"\nOutput 2: {output_2}: \nExpected Output: {output_2}")
