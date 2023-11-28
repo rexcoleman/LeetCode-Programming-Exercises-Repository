3a98cacef001103d5ec8621021380474e55eb729
rexcoleman <rexcoleman@gmail.com>
2023-11-28 10:22:14 -0800

    Commit Multiple Exercises

---
+++ Course-3-Exercise-427-ConstructQuadTree-Recursion-Optimized.py
@@ -0,0 +1,94 @@
+from collections import deque
+from typing import List
+
+
+class Node:
+    def __init__(self, val, isLeaf, topLeft=None, topRight=None, bottomLeft=None, bottomRight=None):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+
+        def construct_range(top, left, l):
+            # Construct a node representing the subgrid with
+            # top left corner at (top, left) and side length l
+            if l == 1:
+                return Node(grid[top][left] == 1, True)
+            topLeft = construct_range(top, left, l // 2)
+            topRight = construct_range(top, left + l // 2, l // 2)
+            bottomLeft = construct_range(top + l // 2, left,  l // 2)
+            bottomRight = construct_range(top + l // 2, left + l // 2, l // 2)
+
+            children = [topLeft, topRight, bottomLeft, bottomRight]
+            # Check if all sub-grids have the same value
+            if all(child.isLeaf and child.val == topLeft.val for child in children):
+                return Node(topLeft.val, True)
+
+            return Node(1, False, topLeft, topRight, bottomLeft, bottomRight)
+
+        return construct_range(0, 0, len(grid))
+
+
+
+
+def quadTreeToList(root):
+    output = []
+    if not root:
+        return output
+    queue = deque([root])
+    while queue:
+        current = queue.popleft()
+        if current is not None:
+            output.append([int(current.isLeaf), int(current.val)])
+            queue.append(current.topLeft)
+            queue.append(current.topRight)
+            queue.append(current.bottomLeft)
+            queue.append(current.bottomRight)
+        else:
+            output.append(None)
+    while output and output[-1] is None:
+        output.pop()
+    return output
+
+
+
+
+
+if __name__ == '__main__':
+
+    # Inputs and Expected Outputs
+    grid_1 = [[0, 1], [1, 0]]
+    expected_output_1 = [[0, 1], [1, 0], [1, 1], [1, 1], [1, 0]]
+    grid_2 = [
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0, 0]
+    ]
+    expected_output_2 = [
+        [0, 1], [1, 1], [0, 1], [1, 1],
+        [1, 0], None, None, None, None,
+        [1, 0], [1, 0], [1, 1], [1, 1]
+    ]
+
+    # Run Tests
+    solution_1 = Solution()
+    solution_2 = Solution()
+    test_1 = solution_1.construct(grid_1)
+    test_2 = solution_2.construct(grid_2)
+
+    # Print Results
+    output_1 = quadTreeToList(test_1)
+    output_2 = quadTreeToList(test_2)
+    print(f"\nTest 1 Result {output_1} \nExpected Result: {expected_output_1}")
+    print(f"\nTest 2 Result {output_2} \nExpected Result: {expected_output_2}")
