349e45c67ad949002274d2b20bf5ca51d22d0dd8
rexcoleman <rexcoleman@gmail.com>
2023-12-11 04:55:00 -0800

    Commit Multiple Exercises

---
+++ Course-3-Exercise-120-Triangle-DynamicProgrammingBottomUpFlipTriangleUpsideDown-AuxiliarySpace.py
@@ -0,0 +1,32 @@
+import math
+from typing import List
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        below_row = triangle[-1]
+        n = len(triangle)
+        for row in reversed(range(n - 1)):
+            curr_row = []
+            for col in range(row + 1):
+                smallest_below = min(below_row[col], below_row[col + 1])
+                curr_row.append(triangle[row][col] + smallest_below)
+            below_row = curr_row
+        return below_row[0]
+
+
+if __name__ == '__main__':
+    # Inputs and Expected Outputs
+    triangle_1 = [[2],[3,4],[6,5,7],[4,1,8,3]]
+    expected_output_1 = 11
+    triangle_2 = [[-10]]
+    expected_output_2 = -10
+
+
+    # Run Tests
+    solution_1 = Solution()
+    solution_2 = Solution()
+    test_1 = solution_1.minimumTotal(triangle_1)
+    test_2 = solution_2.minimumTotal(triangle_2)
+
+    # Print Results
+    print(f"\nTest 1 Result: {test_1} \nExpected Result: {expected_output_1}")
+    print(f"\nTest 2 Result: {test_2} \nExpected Result: {expected_output_2}")
