82f19918812b65b405783b8925ad89c5c655c01b
rexcoleman <rexcoleman@gmail.com>
2023-12-20 09:02:58 -0800

    Commit Multiple Exercises

---
+++ Course-3-Exercise-212-WordSearch2-BacktrackingWithTrie.py
@@ -0,0 +1,84 @@
+from typing import List
+
+
+class Solution:
+    def finWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        WORD_KEY = '$'
+
+        trie = {}
+        for word in words:
+            node = trie
+            for letter in word:
+                # retrieve the next node; If not found, create a empty node.
+                node = node.setdefault(letter, {})
+            # mark the existence of a word in trie node
+            node[WORD_KEY] = word
+
+        rowNum = len(board)
+        colNum = len(board[0])
+
+        matchedWords = []
+
+        def backtracking(row, col, parent):
+
+            letter = board[row][col]
+            currNode = parent[letter]
+
+            # check if we find a match of word
+            word_match = currNode.pop(WORD_KEY, False)
+            if word_match:
+                # we removed the matched word to avoid duplicates,
+                # as well as avoiding using set() for results.
+                matchedWords.append(word_match)
+
+            # Before the EXPLORATION, mark the cell as visited
+            board[row][col] = '#'
+
+            # Explore the neighbors in 4 directions, i.e. up, right, down, left
+            for (rowOffset, colOffset) in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                newRow, newCol = row + rowOffset, col + colOffset
+                if newRow < 0 or newRow >= rowNum or newCol < 0 or newCol >= colNum:
+                    continue
+                if board[newRow][newCol] not in currNode:
+                    continue
+                backtracking(newRow, newCol, currNode)
+
+            # End of EXPLORATION, we restore the cell
+            board[row][col] = letter
+
+        for row in range(rowNum):
+            for col in range(colNum):
+                # starting from each of the cells
+                if board[row][col] in trie:
+                    backtracking(row, col, trie)
+
+        return matchedWords
+
+
+if __name__ == '__main__':
+
+    # Inputs and Expected Outputs
+    board_1 = [
+        ["o", "a", "a", "n"],
+        ["e", "t", "a", "e"],
+        ["i", "h", "k", "r"],
+        ["i", "f", "l", "v"]
+    ]
+    words_1 = ["oath", "pea", "eat", "rain"]
+    expected_output_1 = ["eat", "oath"]
+    board_2 = [
+        ["a", "b"],
+        ["c", "d"]
+    ]
+    words_2 = ["abcb"]
+    expected_output_2 = []
+
+    # Run Tests
+    solution_1 = Solution()
+    solution_2 = Solution()
+    test_1 = solution_1.finWords(board_1, words_1)
+    test_2 = solution_2.finWords(board_2, words_2)
+
+    # Print Results
+    print(f"\nTest 1 Output: {test_1} \nExpected Output: {expected_output_1}")
+    print(f"\nTest 2 Output: {test_2} \nExpected Output: {expected_output_2}")
