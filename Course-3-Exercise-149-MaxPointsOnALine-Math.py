65e8ff3189a09f0b68b324cc3c8d51a31515dcc2
rexcoleman <rexcoleman@gmail.com>
2023-12-22 16:06:11 -0800

    Commit Multiple Exercises

--- Course-3-Exercise-149-MaxPointsOnALine-Math.py
+++ Course-3-Exercise-149-MaxPointsOnALine-Math.py
@@ -1,9 +1,21 @@
+import math
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        pass
+        n = len(points)
+        if n == 1:
+            return 1
+        result = 2
+        for i in range(n):
+            cnt = defaultdict(int)
+            for j in range(n):
+                if j != i:
+                    cnt[math.atan2(points[j][1] - points[i][1], points[j][0] - points[i][0])] += 1
+            result = max(result, max(cnt.values()) + 1)
+        return result
 
 
 if __name__ == '__main__':
