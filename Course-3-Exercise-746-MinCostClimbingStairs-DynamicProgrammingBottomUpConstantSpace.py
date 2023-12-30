051413464cdeda2749593285b37e7ecf41f1f9ae
rexcoleman <rexcoleman@gmail.com>
2023-12-30 07:06:16 -0800

    Commit Multiple Exercises

---
+++ Course-3-Exercise-746-MinCostClimbingStairs-DynamicProgrammingBottomUpConstantSpace.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        down_one = down_two = 0
+        for i in range(2, len(cost) + 1):
+            temp = down_one
+            down_one = min(down_one + cost[i - 1], down_two + cost[i - 2])
+            down_two = temp
+        return down_one
+
+
+if __name__ == '__main__':
+
+    # Inputs and Expected Outputs
+    cost_1 = [10, 15, 20]
+    expected_output_1 = 15
+    cost_2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+    expected_output_2 = 6
+
+    # Run Tests
+    solution_1 = Solution()
+    solution_2 = Solution()
+    test_1 = solution_1.minCostClimbingStairs(cost_1)
+    test_2 = solution_2.minCostClimbingStairs(cost_2)
+
+    # Prnt Results
+    print(f"\nTest 1 Output: {test_1} \nExpected Output: {expected_output_1}")
+    print(f"\nTest 1 Output: {test_2} \nExpected Output: {expected_output_2}")
