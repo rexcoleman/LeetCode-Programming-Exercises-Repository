5f0f95104033f39585b9f5340c9c78f392f5577e
rexcoleman <rexcoleman@gmail.com>
2023-12-30 07:06:48 -0800

    Commit Multiple Exercises

---
+++ Course-3-Exercise-1137-NthTribonnacciNumber-BetterDynamicProgrammingBottomUp.py
@@ -0,0 +1,27 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a, b, c = 0, 1, 1
+        for i in range(3, n + 1):
+            a, b, c = b, c, (a + b + c)
+            if i == n:
+                return c
+
+
+if __name__ == '__main__':
+
+    # Inputs and Expected Outputs
+    n_1 = 4
+    expected_output_1 = 4
+    n_2 = 25
+    expected_output_2 = 1389537
+
+    # Run Tests
+    solution_1 = Solution()
+    solution_2 = Solution()
+    test_1 = solution_1.tribonacci(n_1)
+    test_2 = solution_2.tribonacci(n_2)
+
+    # Prnt Results
+    print(f"\nTest 1 Output: {test_1} \nExpected Output: {expected_output_1}")
+    print(f"\nTest 1 Output: {test_2} \nExpected Output: {expected_output_2}")
+
