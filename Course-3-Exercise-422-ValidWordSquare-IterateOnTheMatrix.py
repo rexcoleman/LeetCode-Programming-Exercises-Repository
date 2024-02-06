0c758097ffb3fd563fa708a8e9b008465682abb2
rexcoleman <rexcoleman@gmail.com>
2024-02-06 12:44:13 -0800

    Commit Multiple Exercises

---
+++ Course-3-Exercise-422-ValidWordSquare-IterateOnTheMatrix.py
@@ -0,0 +1,42 @@
+from typing import List
+
+
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        for word_num in range(len(words)):
+            for char_pos in range(len(words[word_num])):
+                # char_pos (curr 'row' word) is bigger than column word, or
+                # word_num (curr 'column' word) is bigger than row word, or
+                # characters at index (word_num,char_pos) and (char_pos,word_num) are not equal.
+                if char_pos >= len(words) or word_num >= len(words[char_pos]) or words[word_num][char_pos] != words[char_pos][word_num]:
+                    return False
+        return True
+
+
+if __name__ == '__main__':
+
+    # Inputs and Expected Outputs
+    words_1 = ["abcd", "bnrt", "crmy", "dtye"]
+    expected_output_1 = True
+    words_2 = ["abcd", "bnrt", "crm", "dt"]
+    expected_output_2 = True
+    words_3 = ["ball", "area", "read", "lady"]
+    expected_output_3 = False
+    words_4 = ["abc","b","c"]
+    expected_output_4 = True
+
+    # Run Tests
+    solution_1 = Solution()
+    solution_2 = Solution()
+    solution_3 = Solution()
+    solution_4 = Solution()
+    test_1 = solution_1.validWordSquare(words_1)
+    test_2 = solution_2.validWordSquare(words_2)
+    test_3 = solution_3.validWordSquare(words_3)
+    test_4 = solution_4.validWordSquare(words_4)
+
+    # Print Test Results
+    print(f"\nTest 1 Output: {test_1} \nExpected Output: {expected_output_1}")
+    print(f"\nTest 2 Output: {test_2} \nExpected Output: {expected_output_2}")
+    print(f"\nTest 3 Output: {test_3} \nExpected Output: {expected_output_3}")
+    print(f"\nTest 4 Output: {test_4} \nExpected Output: {expected_output_4}")
