c444eceb48a3ee31963bf69c334acfc20d155ab2
rexcoleman <rexcoleman@gmail.com>
2024-02-08 06:51:25 -0800

    Commit Multiple Exercises

---
+++ Course-3-Exercise-346-MovingAverageFromDataStream-CircularQueue.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = [0] * self.size
+        self.head = self.window_sum = 0
+        self.count = 0
+
+    def next(self, val: int) -> float:
+        self.count += 1
+        queue = self.queue
+        hed = self.head
+        tail = (self.head + 1) % self.size
+        self.window_sum = self.window_sum - self.queue[tail] + val
+        window_sum = self.window_sum
+
+        return self.window_sum / min(self.size, self.count)
+
+
+if __name__ == '__main__':
+
+    commands = ["MovingAverage", "next", "next", "next", "next"]
+    inputs = [[3], [1], [10], [3], [5]]
+    expected_output = [None, 1.0, 5.5, 4.66667, 6.0]
+
+    moving_average = MovingAverage(inputs[0][0])
+    output = [None,]
+
+    for _ in range(1, len(commands)):
+        if commands[_] == 'next':
+            result = moving_average.next(inputs[_][0])
+            formatted_result = float("{:.5f}".format(result))
+            output.append(formatted_result)
+
+    print(f"\nTest Output: {output} \nExpected Output: {expected_output}")
