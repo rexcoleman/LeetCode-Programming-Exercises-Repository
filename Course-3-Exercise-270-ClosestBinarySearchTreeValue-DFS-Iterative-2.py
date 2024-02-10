9658e5e889c8325fd724377388b1ec8ea20b1576
rexcoleman <rexcoleman@gmail.com>
2024-02-10 07:20:20 -0800

    Commit Multiple Exercises

--- Course-3-Exercise-270-ClosestBinarySearchTreeValue-DFS-Iterative-2.py
+++ Course-3-Exercise-270-ClosestBinarySearchTreeValue-DFS-Iterative-2.py
@@ -53,47 +53,53 @@ class BinarySearchTree:
 
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        queue = deque([root])
-        min_dist = abs(root.val - target)
-        output = root.val
-        while queue:
-            current = queue.popleft()
-            dist = abs(current.val - target)
-            if dist < min_dist:
-                min_dist = dist
-                output = current.val
-            if current.left is not None:
-                queue.append(current.left)
-            if current.right is not None:
-                queue.append(current.right)
-        return output
+        closest = root.val
+        while root:
+            # Update the closest value if the current node is closer to the target.
+            if abs(root.val - target) < abs(closest - target):
+                closest = root.val
+
+            # Decide whether to go left or right in the tree.
+            root = root.left if target < root.val else root.right
+
+        return closest
 
 
 if __name__ == '__main__':
     # Inputs and Expected Outputs
     root_1 = [4, 2, 5, 1, 3]
     target_1 = 3.714286
-    expected_output_1 = 4
+    expected_output_1 = 3.5
     root_2 = [1]
     target_2 = 4.428571
     expected_output_2 = 1
+    root_3 = [1,None,2]
+    target_3 = 3.428571
+    expected_output_3 = 2
+
 
     # Construct Binary Search Trees
     binarySearchTree_1 = BinarySearchTree()
     binarySearchTree_2 = BinarySearchTree()
+    binarySearchTree_3 = BinarySearchTree()
     bst_1 = binarySearchTree_1.listToBinarySearchTree(root_1)
     bst_2 = binarySearchTree_2.listToBinarySearchTree(root_2)
+    bst_3 = binarySearchTree_3.listToBinarySearchTree(root_3)
 
     # Print To Test Tree Construction
     print(f"\nTree 1 Print Test: {binarySearchTree_1.binarySearchTreeToList(bst_1)} \nExpected Output: {root_1}")
     print(f"\nTree 2 Print Test: {binarySearchTree_2.binarySearchTreeToList(bst_2)} \nExpected Output: {root_2}")
+    print(f"\nTree 3 Print Test: {binarySearchTree_3.binarySearchTreeToList(bst_3)} \nExpected Output: {root_3}")
 
     # Run Tests
     solution_1 = Solution()
     solution_2 = Solution()
+    solution_3 = Solution()
     test_1 = solution_1.closestValue(bst_1, target_1)
     test_2 = solution_2.closestValue(bst_2, target_2)
+    test_3 = solution_3.closestValue(bst_3, target_3)
 
     # Print Test Results
     print(f"\nTest 1 Output: {test_1} \nExpected Output: {expected_output_1}")
     print(f"\nTest 2 Output: {test_2} \nExpected Output: {expected_output_2}")
+    print(f"\nTest 3 Output: {test_3} \nExpected Output: {expected_output_3}")
